37ba0fb199dfc7352bccce2e1cc667eb71a414df
LevPleshkov <levpleshkov@gmail.com>
2023-04-13 14:30:19 +0400

    supply more data to template

--- profile_scraper/views.py
+++ profile_scraper/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Sum
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
@@ -7,14 +8,18 @@ from .models import Hashtag, Location, Post, Profile
 
 def index(request):
     template = loader.get_template('profile_scraper/index.html')
-    profiles_cnt = Profile.objects.count()
-    posts_cnt = Post.objects.count()
-    hashtags_cnt = Hashtag.objects.count()
-    locations_cnt = Location.objects.count()
     context = {
-        'profiles_count': profiles_cnt,
-        'posts_count': posts_cnt,
-        'hashtags_count': hashtags_cnt,
-        'locations_count': locations_cnt,
-    }
+        'scraper_is_active': True,
+        'profiles_count': Profile.objects.count(),
+        'top_profiles_count': Profile.objects.filter(
+            followers__gte=400_000).count(),
+        'posts_count': Post.objects.count(),
+        'posts_scraped_count': Post.objects.aggregate(
+            Sum('scrape_count'))['scrape_count__sum'],
+        'hashtags_count': Hashtag.objects.count(),
+        'hashtags_scraped_count': Hashtag.objects.aggregate(
+            Sum('scrape_count'))['scrape_count__sum'],
+        'locations_count': Location.objects.count(),
+        'locations_scraped_count': Location.objects.aggregate(
+            Sum('scrape_count'))['scrape_count__sum'], }
     return HttpResponse(template.render(context, request))
